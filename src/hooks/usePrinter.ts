f80f46a74817e2a107fae284831f1c77314cbfa6
Art <44448694+HFHT@users.noreply.github.com>
2024-03-01 08:36:28 -0700

    Update usePrinter.ts

--- src/hooks/usePrinter.ts
+++ src/hooks/usePrinter.ts
@@ -26,27 +26,27 @@ export function usePrinter({ name = '_blank', height = 400, width = 600 }: IuseP
                 </head><body >`
             )
             mywindow.document.write(
-                `<div style="width:400px; "><img width="320"\
+                `<div style="width:360px; "><img width="300"\
                 src="${import.meta.env.VITE_STORAGEIMAGEURL}${CONST_LOGO_IMAGE}"/>`
             )
             mywindow.document.write(
                 `<table style="display:flex; align-items:baseline">\
                 <tr style="display:flex; align-items:center "><td>\
-                <img height="80" style="margin-left:1rem;"\
+                <img height="70" style="margin-left:1rem;"\
                 src="${import.meta.env.VITE_STORAGEIMAGEURL}WAS.png"/>\
-                </td><td><h1 style="font-size:4rem; margin-left:4rem; text-decoration-line: underline;">${detail.was}</h1></td></tr>`
+                </td><td><h1 style="font-size:3.8rem; margin-left:4rem; text-decoration-line: underline;">${detail.was}</h1></td></tr>`
             )
             mywindow.document.write(
                 `<tr style="display:flex; align-items:center "><td>\
-                <img height="80" style="margin-left:1rem;"\
+                <img height="70" style="margin-left:1rem;"\
                 src="${import.meta.env.VITE_STORAGEIMAGEURL}NOW.png"/>\
-                </td><td><h1 style="font-size:4rem; margin-left:4rem; text-decoration-line: underline;">${detail.now}</h1></td></tr>`
+                </td><td><h1 style="font-size:3.8rem; margin-left:4rem; text-decoration-line: underline;">${detail.now}</h1></td></tr>`
             )
             mywindow.document.write(
                 `<tr style="display:flex; align-items:center "><td>\
-                <img height="80" style="margin-left:1rem;"\
+                <img height="70" style="margin-left:1rem;"\
                 src="${import.meta.env.VITE_STORAGEIMAGEURL}ITEM.png"/>\
-                </td><td><h1 style="font-size:4rem; margin-left:3rem; text-decoration-line: underline;">${detail.item}</h1></td></tr>\
+                </td><td><h1 style="font-size:3.8rem; margin-left:3rem; text-decoration-line: underline;">${detail.item}</h1></td></tr>\
                 </table>`
             )
             // mywindow.document.write(
@@ -54,7 +54,7 @@ export function usePrinter({ name = '_blank', height = 400, width = 600 }: IuseP
             //     src="${import.meta.env.VITE_STORAGEIMAGEURL}BANGNAIL.png"/>`
             // )
             mywindow.document.write(
-                `<div style="display:flex; justify-content:center; align-items:center; height:100px; ${currentDiscount(theColorIdx)}"><h2 style=" text-align:center">EVERY SALE BANGS A NAIL</h2></div></div>`
+                `<div style="display:flex; justify-content:center; align-items:center; height:80px; ${currentDiscount(theColorIdx)}"><h2 style=" text-align:center">EVERY SALE BANGS A NAIL</h2></div></div>`
             )
             // mywindow.document.write(document.getElementById(elem).innerHTML);
             mywindow.document.write('</body></html>')
