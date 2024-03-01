894b1222c2df39dfd8006c00e1cc5a75c451d45d
Art <44448694+HFHT@users.noreply.github.com>
2024-03-01 09:55:01 -0700

    Update index.ts white background

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { dateFormat } from "../helpers";
 
 export const CONST_DISCOUNTS = ['blue-discount-collection', 'yellow-collection', 'red-collection', 'green-collection']
-export const CONST_DISCOUNT_COLORS = [{ fg: '#000000', bg: '#194b9d' }, { fg: '#FFFFFF', bg: '#FFCC00' }, { fg: '#FFFFFF', bg: '#A63740' }, { fg: '#FFFFFF', bg: '#43B02A' }]
+export const CONST_DISCOUNT_COLORS = [{ fg: '#000000', bg: '#EEEEEE' }, { fg: '#FFFFFF', bg: '#FFCC00' }, { fg: '#FFFFFF', bg: '#A63740' }, { fg: '#FFFFFF', bg: '#43B02A' }]
 export const CONST_DISCOUNT_PCT = [1, 0.75, 0.5, 0.25]
 export const CONST_ARCHIVE_AFTER = 4
 export const CONST_LOGO_IMAGE = 'HabiStorelogo_stacked_black.png'
